daf0a83cff18f8c9d0b92d4df0650ce6fc5f1f87
Donald Twitty <donaldctwittyjr@outlook.com>
2023-08-18 12:22:02 -0500

    completed delete exercises sql

---
+++ delete_exercises.sql
@@ -0,0 +1,14 @@
+SELECT * FROM albums WHERE release_date > 1991;
+
+SELECT * FROM albums WHERE genre = 'Disco';
+
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+
+DELETE FROM albums WHERE release_date > 1991;
+
+DELETE FROM albums WHERE genre = 'Disco';
+
+DELETE FROM albums WHERE artist = 'Michael Jackson';
+
+SELECT * FROM albums;
+
