2d47a487640cf221fbc2e42697f0b8fd5d3ccca2
Donald Twitty <donaldctwittyjr@outlook.com>
2023-08-22 09:27:28 -0500

    updated Group By exercise with AS total

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,9 +1,24 @@
 SELECT DISTINCT title FROM titles;
 
-SELECT last_name FROM employees WHERE last_name LIKE 'E%E%' GROUP BY last_name;
+SELECT last_name
+FROM employees
+WHERE last_name
+LIKE 'E%E%'
+GROUP BY last_name;
 
-SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%E%' GROUP BY first_name, last_name;
+SELECT first_name, last_name
+FROM employees
+WHERE last_name
+LIKE 'E%E%'
+GROUP BY first_name, last_name;
 
-SELECT last_name, COUNT(*) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+SELECT last_name, COUNT(*) AS total
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
 
-SELECT gender, COUNT(*) FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
+SELECT gender, COUNT(*) AS total
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
